b4815d78aed816aa8c7936874c5b6a1405c9d007
Kim._.jazz <jeong011010@naver.com>
2025-08-30 04:47:57 +0900

    feat: 홈페이지 레이아웃

---
+++ src/app/(main)/home/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <main className="pb-24">
+      {/* 검색바 */}
+      {/* 배너 */}
+      {/* 카테고리 */}
+      {/* 리뷰 */}
+      {/* 스토리 */}
+      {/* 벤더 */}
+    </main>
+  );
+}
