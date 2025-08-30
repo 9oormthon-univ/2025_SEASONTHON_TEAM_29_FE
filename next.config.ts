c6bdd6b946f2133aa27600dd5b6f1b080cb6265c
Kim._.jazz <jeong011010@naver.com>
2025-08-31 02:30:58 +0900

    Merge remote-tracking branch 'origin/main' into dev

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ import type { NextConfig } from 'next';
 const backend = process.env.BACKEND_URL?.replace(/\/$/, ''); // 뒤 슬래시 제거(// 방지)
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  eslint: {
+    // 빌드 시 eslint 검사하지 않음 (임시)
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     if (!backend) {
       console.warn('⚠️ BACKEND_URL is not set');
