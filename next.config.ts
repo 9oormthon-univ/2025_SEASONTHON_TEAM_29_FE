c5401ea63a92160cdb604453a667e7cdb500b7d4
Hyunbin Kim <taque88@naver.com>
2025-08-31 00:14:46 +0900

    setting : ESLint 임시 비활성화

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 빌드 시 eslint 검사하지 않음 (임시)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
