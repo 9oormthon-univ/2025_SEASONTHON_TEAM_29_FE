002227bd9e732f45f1534230aa0ad162059d6f82
psm1st <psm1st@sookmyung.ac.kr>
2025-09-05 03:23:22 +0900

    fix : 페이지 구현을 위해 수정

--- src/components/reservation/ReservationButton.tsx
+++ src/components/reservation/ReservationButton.tsx
@@ -18,12 +18,12 @@ export default function ReservationButton({
     <button
       onClick={onClick}
       className={cn(
-        'w-40 h-16 relative rounded-lg overflow-hidden',
+        'w-42 h-16 relative rounded-lg overflow-hidden',
         'flex items-center justify-center text-base font-medium',
         variant === 'default' &&
-          'outline outline-1 outline-offset-[-1px] outline-box-line font-normal',
+          'outline-1 outline-offset-[-1px] outline-box-line font-normal',
         variant === 'primary' &&
-          'outline outline-2 outline-offset-[-2px] outline-primary-500 font-medium',
+          'outline-2 outline-offset-[-2px] outline-primary-500 font-medium',
       )}
     >
       {children}
