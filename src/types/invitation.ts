504fbebb05bde60b7b862c5e3dd3f708bbf900a5
psm1st <psm1st@sookmyung.ac.kr>
2025-09-17 17:08:23 +0900

    feat : thema image 다중 s3 업로드 로직 구현

--- src/types/invitation.ts
+++ src/types/invitation.ts
@@ -131,3 +131,82 @@ export const INVITATION_MOCK: InvitationApi = {
     ),
   },
 };
+
+export type InvitationTheme = {
+  font: string;
+  fontSize: string;
+  accentColor: string;
+  template: 'FILM';
+  canEnlarge: boolean;
+  appearanceEffect: boolean;
+};
+
+export type InvitationBasicInformation = {
+  groomFirstName: string;
+  groomLastName: string;
+  groomFatherName: string;
+  groomMotherName: string;
+  brideFirstName: string;
+  brideLastName: string;
+  brideFatherName: string;
+  brideMotherName: string;
+  groomFatherDead: boolean;
+  groomMotherDead: boolean;
+  brideFatherDead: boolean;
+  brideMotherDead: boolean;
+  brideFirst: boolean;
+};
+
+export type InvitationGreetings = {
+  greetingsTitle: string;
+  greetingsContent: string;
+  greetingsSortInOrder: boolean;
+};
+
+export type InvitationMarriageDate = {
+  marriageDate: string;
+  marriageTime: string;
+  representDDay: boolean;
+};
+
+export type InvitationMarriagePlace = {
+  vendorName: string;
+  floorAndHall: string;
+  drawSketchMap: boolean;
+};
+
+export type InvitationGallery = {
+  galleryTitle: string;
+  arrangement: 'SWIPE';
+  popUpViewer: boolean;
+};
+
+export type InvitationMediaItem = {
+  mediaKey: string;
+  contentType: string;
+  sortOrder: number;
+};
+
+export type InvitationRequestBody = {
+  theme: InvitationTheme;
+  basicInformation: InvitationBasicInformation;
+  greetings: InvitationGreetings;
+  marriageDate: InvitationMarriageDate;
+  marriagePlace: InvitationMarriagePlace;
+  gallery: InvitationGallery;
+  ending: string;
+  account: string;
+  background: string;
+  mainMedia: InvitationMediaItem;
+  filmMedia: InvitationMediaItem[];
+  ticketMedia: InvitationMediaItem;
+  mediaList: InvitationMediaItem[];
+};
+
+export type InvitationResponse<T = { id: string }> = T;
+
+export type MediaItem = {
+  mediaKey: string;
+  contentType: string;
+  sortOrder: number;
+};
